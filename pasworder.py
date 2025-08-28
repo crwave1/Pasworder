7e1aa6280a65230ded9ee766ac9093bb4615d345
crwave1 <demiryusufaslan04@gmail.com>
2025-08-29 01:53:24 +0300

    Added customization options

--- pasworder.py
+++ pasworder.py
@@ -11,34 +11,55 @@ import string
 import random
 
 print()
-print("============== pasworder ==============")
+print("=============================<{ pasworder }>=============================")
 print()
 
 pw = ""
-charlist = string.ascii_letters
-charlist2 = charlist + string.digits
-charlist3 = charlist2 + string.punctuation
-pwlength = int(input("Please enter desired password length: "))
 
 # picking pw complexity
 
-pick = int(input("Please enter desired password complexity (from 1 to 3): "))
+#charlist = string.ascii_letters
+#charlist2 = charlist + string.digits
+#charlist3 = charlist2 + string.punctuation
 
-if pick == 1:
-   pickedlist = charlist
-elif pick == 2:
-   pickedlist = charlist2
-elif pick == 3:
-   pickedlist = charlist3
+#pick = int(input("Please enter desired password complexity (from 1 to 3): "))
+#if pick == 1:
+#   pickedlist = charlist
+#elif pick == 2:
+#   pickedlist = charlist2
+#elif pick == 3:
+#   pickedlist = charlist3
 
-# will add try block for 'int(input())' lines
+chlist = ""
+
+if input("Would you like to provide a specific character set? (y/n) ") == "y":
+   chlist = input("Please input the characters you want in your password: ")
+
+elif input("Would you like numbers in your password? (y/n) ") == "y":
+   chlist = string.digits
+
+   if input("Would you like lowercase letters in your password? (y/n) ") == "y":
+      chlist += string.ascii_lowercase
+
+   if input("Would you like uppercase letters in your password? (y/n) ") == "y":
+      chlist += string.ascii_uppercase
+
+   if input("Would you like basic symbols (!, ?, .) in your password? (y/n) ") == "y":
+      chlist += "!?."
+
+   if input("Would you like an extreme password, disregarding previous choices? (y/n) ") == "y":
+      chlist = string.digits + string.ascii_letters + string.punctuation
+
+pwlength = int(input("Please enter desired password length: "))
+
+# might add try block for 'int(input())' lines
 
 print()
 
 i = 0
 
 while i < pwlength:
-   pw = pw + random.choice(pickedlist)
+   pw = pw + random.choice(chlist)
   # print(pw)                                         # FOR SEEING THE PASSWORD CREATION PROCESS ONLY. REMOVE LATER
    i = i+1
 
